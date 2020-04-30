7cba90c918f0b1bdd40448702ba0219a9d74dbf6
sidtrip <39147264+sidtrip@users.noreply.github.com>
2020-04-30 19:53:29 +0530

    Create multiplyNumbers.py

---
+++ CS106A/week2/ass2helpsess/multiplyNumbers.py
@@ -0,0 +1,16 @@
+
+def main():
+    #read two integers
+    num1 = float(input("Enter first number "))
+    num2 = float(input("Enter second number "))
+    #convet to float
+
+    #multiply them
+    multiple = num1 * num2
+
+    #print result
+    print("The multiplication is: " + str(multiple) + ".")
+
+
+if __name__ == '__main__':
+    main()
