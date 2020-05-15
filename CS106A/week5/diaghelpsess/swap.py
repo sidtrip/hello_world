e31e27fcc07ac299834068766aefb2625482390e
sidtrip <39147264+sidtrip@users.noreply.github.com>
2020-05-15 22:24:06 +0530

    Create swap.py

---
+++ CS106A/week5/diaghelpsess/swap.py
@@ -0,0 +1,15 @@
+def main():
+    curr_player = "maya"
+    next_player = "ali"
+    #goal:swap these tow variables.
+    save_slot = curr_player
+    curr_player = next_player
+    next_player = save_slot
+
+    #non decreasing
+    
+
+
+
+if __name__ == "__main__":
+    main()
