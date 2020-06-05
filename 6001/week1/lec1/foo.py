ced2998486157fd649af305208baa40ccaeccc83
Siddhant T <tripathisiddhant@gmail.com>
2020-06-05 17:26:11 +0530

    starting mit6001 wk1

---
+++ 6001/week1/lec1/foo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+x = 6
+
+if x != 5:
+    print('i am here')
+else:
+    print('no I am not')
---
+++ 6001/week1/lec1/nestedConds.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+x = input('enter a number')
+if x % 2 == 0:
+    if x % 3 == 0:
+        print('Divisible by 2 and 3')
+    else:
+        print('Divisible by 2 and not by 3')
+elif x % 3 == 0:
+    print('Divisible by 3 and not by 2')
