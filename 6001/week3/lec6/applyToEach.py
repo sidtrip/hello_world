fc94473172777cfc94423cfc34ab322c8940931f
Siddhant T <tripathisiddhant@gmail.com>
2020-07-02 08:54:18 +0530

    lec6

---
+++ 6001/week3/lec6/applyToEach.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+def applyToEach(L, f):
+    """assumes L is a list, f a function
+       mutates L by replacing each element,
+       e, of L by f(e)"""
+    for i in range(len(L)):
+        L[i] = f(L[i])
+        
+
+def applyFuns(L, x):
+    for f in L:
+         print(f(x))
+
