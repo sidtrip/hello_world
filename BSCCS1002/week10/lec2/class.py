f5bb1733e605158eb0cc28bd9562c6e67218e2e4
Siddhant Tripathi <tripathisiddhant@gmail.com>
2021-07-20 17:52:19 +0530

    init of w10

---
+++ BSCCS1002/week10/lec2/class.py
@@ -0,0 +1,24 @@
+class Student:
+    roll_no = None
+    name = None
+
+s0 = Student()
+
+s0.roll_no = 0
+s0.name = 'Bhuvanesh'
+
+print(s0.roll_no, s0.name)
+
+s1 = Student()
+print(s1.roll_no, s1.name)
+
+s2 = Student()
+s2.roll_no = 2
+s2.name = 'Harish'
+print(s2.roll_no, s2.name)
+
+s50 = Student()
+s50.name = 'Asmita'
+print(s50.roll_no, s50.name)
+
+
